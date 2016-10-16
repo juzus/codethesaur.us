0259c318f85772b667107c9940d0ecf27c07f36f
Sarah Withee <github@sarahwithee.com>
2016-10-16 03:20:54 -0500

    Add controllers. Add views. Configure both

---
+++ src/resources/views/home.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Code Thesaurus</title>
+    <meta name="description" content="Code Thesaurus" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="generator" content="Codeply">
+
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    {{--<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" />--}}
+    <link rel="stylesheet" href="css/styles.css" />
+
+    <script src="//code.jquery.com/jquery-3.3.7.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+</head>
+<body>
+
+
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#?logo">
+                    <img alt="Logo" src="img/logo.png" width="32"/>
+                </a>
+                <a class="navbar-brand" href="#?brand">Code Thesaurus</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="compare/">Compare</a></li>
+                    <li><a href="reference/">Reference</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid">
+        Testing more stuff
+    </div>
+
+    <footer class="container-fluid">
+        <p class="small">Copyright &copy; <?php echo date('Y'); ?> Sarah Withee. Check it out on <a href="https://github.com/codethesaurus/codethesaur.us/">GitHub</a>.</p>
+    </footer>
+
+
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -11,20 +11,35 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
-Route::get('languageTypes', function () {
-    return App\LanguageType::all();
-});
+Route::resource('/', 'HomeController');
 
-Route::get('languages', function () {
-    return App\Language::all();
-});
+Route::resource('compare', 'CompareController');
+
+Route::resource('reference', 'ReferenceController');
 
 Route::group(['prefix' => 'api'], function() {
-    Route::get('categories', ['as' => 'categories', function () {
+    Route::get('categories', function () {
         return App\Category::all();
-    }]);
+    });
+
+    Route::get('languageTypes', function () {
+        return App\LanguageType::all();
+    });
+
+    Route::get('languages', function () {
+        return App\Language::all();
+    });
+
+    Route::get('examples', function () {
+        return App\Example::all();
+    });
+
+    Route::get ('example_code', function () {
+        return App\ExampleCode::all();
+    })  ;
 });
+
